e8b93e2eaa58b833001693506e9f6e0c9b3c3a17
Matilde De Jesus Rosario De Oliveira E Costa <matde-je@c2r7s4.42lisboa.com>
2024-01-09 18:39:17 +0000

    ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -2,11 +2,68 @@
 
 Fixed::Fixed(void)
 {
+    std::cout << "Default constructor called" << std::endl;
+    this->fixed_point_value = 0;
     return ;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    this->fixed_point_value = copy.getRawBits();
+    return ;
+}
+
+Fixed &Fixed::operator = (const Fixed &copy)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->fixed_point_value = copy.getRawBits();
+    return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+    return (this->fixed_point_value);
+}
+
+Fixed::Fixed(const int i)
+{
+    std::cout << "Int constructor called"<< std::endl;
+    fixed_point_value = i << fractional_bits;
+}
+
+Fixed::Fixed(const float i)
+{
+    std::cout << "Float constructor called"<< std::endl;
+    fixed_point_value = (int)roundf(i * (1 << fractional_bits));
+}
+
+//converts the fixed-point value to a floating-point value
+float    Fixed::toFloat(void) const
+{
+    float f;
+
+    f = (float)fixed_point_value / (1 << fractional_bits);
+    return f;
+}
+
+float    Fixed::toInt(void) const
+{
+    int i;
+
+    i = fixed_point_value / (1 << fractional_bits);
+    return i;
+}
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fix) //declaration of << operator overload, takes a reference to an output stream 
+{
+	os << fix.toFloat(); //insert the value of the float into the stream
+	return os; //returns reference to the output stream allowing chaining of << operators
 }
 
 Fixed::~Fixed()
 {
+    std::cout << "Destructor called" << std::endl;
     return ;
 }
 
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -1,18 +1,33 @@
 
-
 #ifndef Fixed_HPP
 #define Fixed_HPP
 
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
+#include <cmath>
+
+//A constructor that takes a constant floating-point number as a parameter.
 
 class Fixed {
+	private:
+        int fixed_point_value;
+        static const int fractional_bits = 8;
 
 	public:
 		Fixed(void);
+		Fixed(const int i);
+		Fixed(const float i);
 		~Fixed(void);
+		Fixed(const Fixed &copy); //copy constructor
+        Fixed &operator = (const Fixed &copy); //assignment operation
+
+        int getRawBits(void) const;
+
+		float toFloat( void ) const;
+		float    toInt(void) const;
 };
 
+std::ostream &operator<<(std::ostream &os, const Fixed &fix);
 
 #endif
